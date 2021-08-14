31e5dff482616e422194efebd10d0db279862b27
Matthew R <mattras82@gmail.com>
2021-08-14 14:10:50 -0600

    Updating sample compose, several fixes in entrypoint for URL formatting, plugin versioning, XDebug update to v3, and setup process

--- wp-docker-entrypoint.sh
+++ wp-docker-entrypoint.sh
@@ -21,6 +21,29 @@ chown www-data:www-data wp-content/plugins
 # with needed permissions
 mkdir -p /var/www/temp
 chown www-data:www-data /var/www/temp
+# Creating WordFence logs directory
+# with needed permissions
+mkdir -p wp-content/wflogs
+chown -R www-data:www-data wp-content/wflogs
+
+setup_local_url() {
+    if [[ "$LOCAL_PORT" = "80" ]]; then
+        echo "$LOCAL_URL";
+    else
+        echo "$LOCAL_URL:$LOCAL_PORT";
+    fi
+}
+
+url=$(setup_local_url)
+
+update_url_in_db() {
+    wp db query "UPDATE ${WORDPRESS_TABLE_PREFIX}options SET option_value = '$url' WHERE option_name IN ('home', 'siteurl');" --allow-root
+}
+
+update_theme_in_db() {
+    wp db query "UPDATE ${WORDPRESS_TABLE_PREFIX}options SET option_value = 'twentytwenty' WHERE option_name IN ('template', 'stylesheet');" --allow-root
+    wp theme activate --allow-root $THEME_NAME
+}
 
 install_github_plugin() {
     echo >&2
@@ -34,7 +57,12 @@ setup_dependencies() {
     # Force the active theme to WP core while we install the PF Parent theme
     wp db query "UPDATE ${WORDPRESS_TABLE_PREFIX}options SET option_value = 'twentytwenty' WHERE option_name IN ('template', 'stylesheet');" --allow-root
 
-    install_github_plugin afragen github-updater
+    # Remove unneeded default plugins
+    wp plugin delete --allow-root akismet hello
+
+    # This plugin has had a major update (for the worse)
+    # So we're hardcoding the version for now
+    wp plugin install https://github.com/afragen/git-updater/releases/download/9.9.10/github-updater-9.9.10.zip --activate --allow-root
 
     install_github_plugin mattras82 pf-wp-toolkit
 
@@ -46,28 +74,31 @@ setup_dependencies() {
 
     chown -R www-data:www-data /var/www/html/wp-content/plugins
 
-    wp plugin install --allow-root $PLUGINS_LIST
+    wp plugin install --allow-root --activate $PLUGINS_LIST
 
     chown -R www-data:www-data /var/www/html/wp-content/themes
 
     # Now that our dependencies are installed, let's reactivate our site's theme
     wp theme activate $THEME_NAME --allow-root
+
+    echo >&2
+    echo >&2 "Initial dependencies installed successfully"
 }
 
 pull_remote_db() {
     if [[ -v REMOTE_DB_PASSWORD && ! -z "$REMOTE_DB_PASSWORD" ]]; then
 		echo >&2
 		echo >&2 "Syncing local database from remote";
-        wp db drop --allow-root --yes
 		echo >&2
 		echo >&2 "Pulling remote database now. Please be patient...";
 		mysqldump -h $REMOTE_DB_HOST --no-tablespaces --user=$REMOTE_DB_USER --password=$REMOTE_DB_PASSWORD $REMOTE_DB_NAME > /tmp/dump.sql
+        wp db drop --allow-root --yes
 		echo >&2
 		echo >&2 "Remote database has been pulled. Recreating local DB now.";
 		wp db create --allow-root
 		wp db import /tmp/dump.sql --allow-root
         rm /tmp/dump.sql
-		wp db query "UPDATE ${WORDPRESS_TABLE_PREFIX}options SET option_value = '$LOCAL_URL' WHERE option_name IN ('home', 'siteurl');" --allow-root
+		update_url_in_db
         echo >&2
         echo >&2 'Local DB synced from remote. Data is ready to roll';
     else
@@ -75,6 +106,11 @@ pull_remote_db() {
         echo >&2;
         echo >&2 'Please add the REMOTE_DB_* environment variables in the .env file';
     fi
+}
+
+setup_xdebug_ini() {
+    echo "xdebug.mode=${XDEBUG_MODE}" >> php.ini;
+    echo "xdebug.start_with_request=${XDEBUG_START_WITH_REQUEST}" >> php.ini;
 }
 
 if [[ ! -e first_run ]]; then
@@ -92,17 +128,22 @@ if [[ ! -e first_run ]]; then
     pull_remote_db
     # Install dependencies
     setup_dependencies
+    # Setup php.ini file so that Xdebug works properly
+    setup_xdebug_ini
     # Create a file so we know it's been run before
     touch first_run
-elif [[ "$REMOTE_DB_IMPORT" = "y" ]]; then
+elif [[ "$REMOTE_DB_IMPORT" = "y" || "$REMOTE_DB_IMPORT" = "Y" ]]; then
     pull_remote_db
+    # Fix for remote site not using the same theme as local
+    update_theme_in_db
 else
     echo >&2
     echo >&2 "Skipping DB import"
+    update_url_in_db
 fi
 
 echo >&2
-echo >&2 "Your website is ready to go at $LOCAL_URL"
+echo >&2 "Your website is ready to go at $url"
 echo >&2
 echo >&2 "Happy Coding! :)"
 
